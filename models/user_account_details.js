f9cea560f241dd48b5f64137b9dea2aeb9951f1d
unknown <idrisadeyiga@gmail.com>
2023-03-15 14:25:51 +0100

    Added account details Schema

--- models/user_account_details.js
+++ models/user_account_details.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose')
+
+const accountSchema = new mongoose.Schema({
+    like: followers,
+    posts: Number,
+    following: String
+})
+
+const account = new mongoose.model('accountdetail', accountSchema)
+
+module.exports = account
